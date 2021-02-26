5e940afa5fcb87d9b9c95831cefdd33ef78e522e
Martha <mleccles@outlook.com>
2021-02-26 15:59:29 +0000

    starts basic styling with Styled Components

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,35 +1,61 @@
-import React, {useContext}from "react";
+import React, { useContext } from 'react';
 import UserContext from '../Context/UserContext';
-import { Link } from "@reach/router";
-import homeButton from "../Images/homebutton.png";
-
+import { Link } from '@reach/router';
+import homeButton from '../Images/homebutton.png';
+import styled from "styled-components";
 function Header() {
-	const {  user, setUser } = useContext(UserContext);
+	const { user, setUser } = useContext(UserContext);
 
-  return (
-    <div className="Header">
-      <p style={{color: `white`}}>Logged in as: {user.username}</p>
-      <Link to="/">
-        {" "}
-        <img
-          src={homeButton}
-          alt="home button"
-          height="50"
-          width="50"
-          className="home-button"
-        />{" "}
-      </Link>
-<button onClick={()=>{
-  setUser(    {
-    username: 'guest',
-    name: 'guest',
-    avatar: ''
- })
-  alert(`you are logging out`)}}>
-  LOGOUT
-</button>
-      <span></span>
-    </div>
-  );
+	return (
+		<StyledNav>
+      {/* <ul  className='header'> */}
+			<Link to='/' className="header__link">
+				{' '}
+				<img
+					src={homeButton}
+					alt='home button'
+					height='50'
+					width='50'
+					className='header__link'
+				/>{' '}
+			</Link>
+			<p>Logged in as: {user.username}</p>
+			<StyledButton
+      className="header__logout-btn"
+				onClick={() => {
+					setUser({
+						username: 'guest',
+						name: 'guest',
+						avatar: '',
+					});
+					console.log('reset to guest');
+				}}>
+				LOGOUT
+			</StyledButton>
+      {/* </ul> */}
+		</StyledNav>
+	);
 }
 export default Header;
+
+/*
+STYLED COMPONENTS
+
+- two styled containers to keep article button and home together and same for login? 
+
+*/
+const StyledNav = styled.nav`
+display: flex;
+padding: 0.5rem;
+flex-flow: row wrap;
+justify-content: space-between;
+background-color: orange;
+`;
+
+
+const StyledButton = styled.button`
+  background-color: black;
+  font-size: inherit;
+  color: white;
+  `;
+
